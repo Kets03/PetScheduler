22f7677f643f6238df5ea82fc1b859e84a00ebf1
Kets03 <narkhedeketakij@gmail.com>
2024-11-15 13:15:40 +0530

    .

--- PetScheduler/settings.py
+++ PetScheduler/settings.py
@@ -27,7 +27,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 SECRET_KEY = 'django-insecure-^r52m2!fh3@e^^ynjje&p-am8t2ntxok97deikvwv!uys186%('
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['care.onrender.com', 'localhost']
 
